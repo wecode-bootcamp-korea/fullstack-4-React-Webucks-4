277307f6649d85c2dffad8121b06e3bd5e07a698
0teklee <leetekwoo@gmail.com>
2022-03-02 11:47:22 +0900

    Add : 4팀 first commit. 초기 세팅 완료.

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
