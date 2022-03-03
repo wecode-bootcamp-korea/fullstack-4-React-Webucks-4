b21835226fa19a636eff53ff52156d659ee2f37f
nsoarim <99406537+nsoarim@users.noreply.github.com>
2022-03-03 23:05:30 +0900

    Add : Mission 2 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/Gyeonghunim/Login/Login.js
+++ src/pages/Gyeonghunim/Login/Login.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 import './Login.scss'
 import logoImage from '../resource/logo.png';
 
 const LoginGyeonghun = () => {
+  const [inputData, setInputData] = useState(0);
+  
+function handleIdInput(event){
+setInputData(event.target.value)
+// console.log(inputData)
+}
+
   return (
   <div className="login_case"><img className="logo" src={logoImage} />
       <form action="/examples/media/action_target.php">
-        <input type="text" id="idInput" className="login_id margin_top" placeholder="전화번호, 사용자 이름 또는 이메일" /><br />
+        <input onChange={handleIdInput} type="text" id="idInput" className="login_id margin_top" placeholder="전화번호, 사용자 이름 또는 이메일" /><br />
         <input type="password" id="pwInput" className="login_password margin_top" placeholder="비밀번호" /><br />
         <button className="login_button margin_top"  type="button" disabled="true" onclick="location.href='list.html'">로그인</button>
         <p className="question">비밀번호를 잊으셨나요?</p>
